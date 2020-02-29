f8769e210952f726f20ea80c21434f8b72ce80d5
Jeffrey Zhang <jeffreyzhang01@gmail.com>
2020-02-29 14:58:49 -0600

    Create models for account and account type

--- app/models.py
+++ app/models.py
@@ -8,11 +8,13 @@ def load_user(id):
     return User.query.get(int(id))
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     #email = db.Column(db.String(120), index=True, unique=True)
     customer_id = db.Column(db.String(24), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    accounts = db.relationship('Account', backref='user')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -22,3 +24,25 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Account(db.Model):
+    __tablename__ = 'accounts'
+    id = db.Column(db.Integer, primary_key=True)
+    account_id = db.Column(db.String(24), index=True, unique=True)
+    nickname = db.Column(db.String())
+    rewards = db.Column(db.Integer)
+    balance = db.Column(db.Integer)
+    user_id = db.Column(db.String(24), db.ForeignKey('users.id'))
+    account_type_id = db.Column(db.String(24), db.ForeignKey('account_types.id'))
+
+    def __repr__(self):
+        return '<Account {}>'.format(self.account_id)
+    
+class AccountType(db.Model):
+    __tablename__ = 'account_types'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(24), unique=True)
+    accounts = db.relationship('Account', backref='account_type')
+
+    def __repr__(self):
+        return '<{} Account>'.format(self.name)

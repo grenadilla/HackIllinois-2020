5c3db47dbe8bfcd59fbf5d250dfae1e0e64005fe
Rem Yang <remyang2@illinois.edu>
2020-02-29 11:24:01 -0600

    Move api key to top of file

--- customers.py
+++ customers.py
@@ -1,8 +1,10 @@
 import requests
 import json
 
+api_key = "5e069ae4f90746158075478b37e7d4b6"
+
 def get_customers():
-    url = "http://api.reimaginebanking.com/customers?key={}"
+    url = "http://api.reimaginebanking.com/customers?key={}".format(api_key)
     response = requests.get(url, headers={'content-type':'application/json'})
     if response.status_code == 201:
 	    print('account created')

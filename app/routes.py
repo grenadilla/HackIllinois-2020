7cfbf0456d64ed494a564dc2740a7054adc8d0ef
Jeffrey Zhang <jeffreyzhang01@gmail.com>
2020-02-29 10:47:58 -0600

    Set up basic login form

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,17 @@
 from app import app
-from flask import jsonify, request, render_template
+from flask import jsonify, request, render_template, redirect, flash
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("hello.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)

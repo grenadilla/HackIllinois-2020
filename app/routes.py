57d011740f6584e6c95c45c8e8b152529651fd24
stevenxpan <stevenp6@illinois.edu>
2020-02-29 11:08:36 -0600

    Pull account data

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,12 @@
 from app import app
 from flask import jsonify, request, render_template
+import customers
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("hello.html")
+
+@app.route('/request')
+def request():
+    return customers.request();
